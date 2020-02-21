1a505f5a49318d58889e7f3fecc28e653bcc200a
Mike <mltefive@gmail.com>
2020-02-21 12:19:21 +0900

    russian

---
+++ locale/ru/auth.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    'full_name'        => 'Полное имя',
+    'email'            => 'Email',
+    'password'         => 'Пароль',
+    'confirm_password' => 'Пароль подтверждение',
+    'remember_me'      => 'Запомнить меня',
+    'sign_in'          => 'Вход',
+    'sign_out'         => 'Выход',
+    'register'         => 'Регистрация',
+
+    'login' => [
+        'title'               => 'Sign in to start your session',
+        'forgot_password'     => 'Я забыл пароль',
+        'register_membership' => 'Register a new membership',
+    ],
+
+    'registration' => [
+        'title'           => 'Register a new membership',
+        'i_agree'         => 'Я согласен',
+        'terms'           => 'the terms',
+        'have_membership' => 'I already have a membership',
+    ],
+
+    'forgot_password' => [
+        'title'          => 'Enter Email to reset password',
+        'send_pwd_reset' => 'Send Password Reset Link',
+    ],
+
+    'reset_password' => [
+        'title'         => 'Сбросить мой Пароль',
+        'reset_pwd_btn' => 'Сбросить Пароль',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Нажмите здесь для сброса пароля',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Участник с',
+        'messages'     => 'Сообщение',
+        'settings'     => 'Настройки',
+        'lock_account' => 'Lock Account',
+        'profile'      => 'Профиль',
+        'online'       => 'В сети',
+        'search'       => 'Посик',
+        'create'       => 'Создать',
+        'export'       => 'Экспорт',
+        'print'        => 'Печать',
+        'reset'        => 'Сброс',
+        'reload'       => 'Обновить',
+    ],
+];
---
+++ locale/ru/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => ':model найдена удачно.',
+    'saved'     => ':model сохранена удачно.',
+    'updated'   => ':model обновлена удачно.',
+    'deleted'   => ':model удалена удачно.',
+    'not_found' => ':model не найдена',
+
+];
